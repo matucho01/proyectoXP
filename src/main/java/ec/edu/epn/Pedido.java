7e730bb0d7b7ba7fc6202893f46cbbdd915d6dcf
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-01-08 16:44:43 -0500

    Historia de usuario 10

---
+++ src/main/java/ec/edu/epn/Pedido.java
@@ -0,0 +1,48 @@
+package ec.edu.epn;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+public class Pedido {
+    private float galonesSuper;
+    private float galonesExtra;
+    private float galonesDiesel;
+    private String fechaPedido;
+
+    public Pedido(ArrayList<Float> totalGalonesPedido, String fecha) {
+        this.galonesSuper = totalGalonesPedido.get(0);
+        this.galonesExtra = totalGalonesPedido.get(1);
+        this.galonesDiesel = totalGalonesPedido.get(2);
+        this.fechaPedido = fecha;
+    }
+
+    public String crearPedido() {
+        String pedido = "";
+        pedido = "El pedido es:" +
+                "\nFecha: " + this.fechaPedido +
+                "\nSuper: " + this.galonesSuper + "00" +
+                "\nExtra: " + this.galonesExtra + "00" +
+                "\nDiesel: " + this.galonesDiesel + "00" +
+                "\nRevisado por: Gerente";
+        return pedido;
+    }
+    public boolean generarArchivo() {
+        try {
+            String nombreArchivo = "pedido"+this.fechaPedido+".txt";
+            File archivo = new File(nombreArchivo);
+            if(archivo.createNewFile()) {
+                System.out.println("Archivo creado exitosamente");
+                FileWriter fw = new FileWriter(nombreArchivo);
+                String pedido = crearPedido();
+                fw.write(pedido);
+                fw.close();
+                return true;
+            }
+        } catch (IOException e) {
+            System.out.println("No se puede crear el archivo con el pedido, error: ");
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
