0849ff0db7436cb79ed72173c1c3193df15e6d8a
Mateo <promateo2001@gmail.com>
2023-01-08 12:23:51 -0500

    Historia de usuario 12 y pruebas completo

--- src/test/java/ec/edu/epn/ReporteTurnoTest.java
+++ src/test/java/ec/edu/epn/ReporteTurnoTest.java
@@ -1,5 +1,6 @@
 package ec.edu.epn;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,28 +9,32 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class ReporteTurnoTest {
-
-    ReporteTurno reporteTurno = new ReporteTurno();
+    ReporteTurno reporteTurno;
+    @Before
+    public void setUp(){
+        reporteTurno = new ReporteTurno();
+        reporteTurno.ingresoEfectivoBilletes(1,1,8,12,21,26);
+        reporteTurno.ingresoEfectivoMonedas(105,50,120,150,73,89);
+    }
     @Test
     public void given_six_integers_when_inserting_number_bills_then_ok(){
-        reporteTurno.ingresoEfectivoBilletes(1,1,8,12,21,26);
+
         ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1,1,8,12,21,26));
         assertArrayEquals(expected.toArray(),reporteTurno.getBilletes().toArray());
     }
 
     @Test
     public void given_six_integers_when_inserting_number_coins_then_ok(){
-        reporteTurno.ingresoEfectivoMonedas(105,50,120,150,73,89);
+
         ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(105,50,120,150,73,89));
         assertArrayEquals(expected.toArray(),reporteTurno.getMonedas().toArray());
     }
 
     @Test
-    public void given_when_then_ok(){
-        reporteTurno.ingresoEfectivoBilletes(1,1,8,12,21,26);
-        reporteTurno.ingresoEfectivoMonedas(105,50,120,150,73,89);
+    public void given_numbers_bills_coins_when_calculate_cash_then_ok(){
         reporteTurno.calcularEfectivo();
-        double expected = 12;
-        assertEquals(expected,reporteTurno.getVentasEfectivo());
+        double expected = 740.54;
+        assertEquals(expected,reporteTurno.getVentasEfectivo(),0.01);
     }
+
 }
