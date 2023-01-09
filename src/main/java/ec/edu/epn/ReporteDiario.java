ac1f033a0dc64c94852f9e608f7b679c53a95e79
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-01-08 19:34:22 -0500

    Commit de prueba

---
+++ src/main/java/ec/edu/epn/ReporteDiario.java
@@ -0,0 +1,13 @@
+package ec.edu.epn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReporteDiario {
+
+    public void bralan() {
+        System.out.println("ola");
+    }
+
+
+}
