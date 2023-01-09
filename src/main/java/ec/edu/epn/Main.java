83a2b7c57067979e6f58d9242b42de53a3f6cee0
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-01-09 07:44:40 -0500

    Pruebas unitarias PedidoTest

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -117,6 +117,6 @@ public class Main {
         System.out.println("Ingrese el nombre del archivo a visualizar: ");
         respuesta = sc.nextLine();
         gestorReporte.mostrarReporte(respuesta);
-        System.out.println("hola1");
+        System.out.println("hola122");
     }
 }
