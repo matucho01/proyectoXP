d0ab2f593b49e02283e75e0a96387c6a92608afa
Mateo <promateo2001@gmail.com>
2023-01-08 22:07:06 -0500

    Historia de usuario 20

---
+++ src/main/java/ec/edu/epn/GestorReporte.java
@@ -0,0 +1,38 @@
+package ec.edu.epn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GestorReporte {
+    private String[] archivos;
+    public void listarPedidos() {
+        File dir = new File("/reportes");
+        this.archivos = dir.list();
+        if (archivos == null)
+            System.out.println("No hay archivos en el directorio especificado");
+        else {
+            for (int i=0; i<archivos.length; i++)
+                System.out.println(archivos[i]);
+        }
+    }
+
+    public void mostrarReporte(String nombreArchivo) {
+        for(int i=0; i<this.archivos.length; i++) {
+            if(nombreArchivo.equals(archivos[i])) {
+                try {
+                    File f = new File(nombreArchivo);
+                    Scanner myReader = new Scanner(f);
+                    while (myReader.hasNextLine()) {
+                        String data = myReader.nextLine();
+                        System.out.println(data);
+                    }
+                    myReader.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println("Existió un error al intentar leer el archivo");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
