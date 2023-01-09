5450d3ff9154d83009354c82a1efe4cc62d466b4
Mateo <promateo2001@gmail.com>
2023-01-08 23:08:14 -0500

    Pruebas Unitarios de LectorDatos

--- src/test/java/ec/edu/epn/LectorDatosTest.java
+++ src/test/java/ec/edu/epn/LectorDatosTest.java
@@ -7,10 +7,9 @@ import java.util.Arrays;
 
 import static org.junit.Assert.*;
 public class LectorDatosTest {
-
+    LectorDatos lectorDatos = new LectorDatos();
     @Test
     public void given_a_list_of_turns_when_comparing_then_ok() {
-        LectorDatos l = new LectorDatos();
         Turno turno1 = new Turno("02/10/22", 541245.23, 789543.55,
                 354765.65, 541378.43, 787678.23, 354801.41, 0);
         Turno turno2 = new Turno("02/10/22", 541378.43, 787678.23,
@@ -18,9 +17,17 @@ public class LectorDatosTest {
         Turno turno3 = new Turno("02/10/22", 541478.43, 787778.23,
                 354901.41, 541578.43, 787878.23, 355001.41, 2);
         ArrayList<Turno> expected = new ArrayList<>(Arrays.asList(turno1, turno2, turno3));
-        ArrayList<Turno> actual = l.obtenerLecturasPorTurno();
+        ArrayList<Turno> actual = lectorDatos.obtenerLecturasPorTurno();
         for(int i=0; i< expected.size(); i++) {
             assertEquals(expected.get(i).toString(), actual.get(i).toString());
         }
     }
+    @Test
+    public void given_list_prices_when_comparing_then_ok(){
+        ArrayList<Double> actual = lectorDatos.obtenerPreciosActualizados();
+        ArrayList<Double> expected = new ArrayList<>(Arrays.asList(4.09,2.40,1.70));
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+
 }
